51f72e6229854bb65d2e60fa9ec88eaa3deb0d20
adrianstephens <adrianstephens@msn.com>
2026-05-03 14:08:15 -0700

    moved bitmap viewer to its own extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 import * as vscode from 'vscode';
 
 import { ExplorerProvider } from './Explorer';
-import { BitmapViewerProvider } from './BitmapViewer';
 import { ZipFileSystem } from './ArchiveFilesystem';
 
 export function activate(context: vscode.ExtensionContext) {
 	const zipFileSystem = new ZipFileSystem(context);
 	new ExplorerProvider(context, zipFileSystem);
-	new BitmapViewerProvider(context);
 }
 
 export function deactivate() {
